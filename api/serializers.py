bbcb20ce1cb820d5ba5cd5e6a5cf6204a30fc946
Raphaël JARREAU <raphael.tr.jarreau@gmail.com>
2024-02-16 11:33:57 +0100

    Modification of the views in api

---
+++ api/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from app.models import Club_2, Club
+from api.models import D_AgeGrp, D_Date, D_Federation, D_Localisation, D_Sex, D_Type, F_Club, F_Licence
+
+class ClubSerializer(serializers.ModelSerializer):
+    """..."""
+    class Meta:
+        """..."""
+        model = Club
+        fields = '__all__'
+
+class Club2Serializer(serializers.ModelSerializer):
+    """..."""
+    class Meta:
+        """..."""
+        model = Club_2
+        fields = '__all__'
+
+class FLicenceSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle F_Licence."""
+    class Meta:
+        model = F_Licence
+        fields = '__all__'
+
+class FClubSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle F_Club."""
+    class Meta:
+        model = F_Club
+        fields = '__all__'
+
+class DAgeGrpSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle D_AgeGrp."""
+    class Meta:
+        model = D_AgeGrp
+        fields = '__all__'
+
+class DDateSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle D_Date."""
+    class Meta:
+        model = D_Date
+        fields = '__all__'
+
+class DFederationSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle D_Federation."""
+    class Meta:
+        model = D_Federation
+        fields = '__all__'
+
+class DLocalisationSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle D_Localisation."""
+    class Meta:
+        model = D_Localisation
+        fields = '__all__'
+
+class DSexSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle D_Sex."""
+    class Meta:
+        model = D_Sex
+        fields = '__all__'
+
+class DTypeSerializer(serializers.ModelSerializer):
+    """Serializer pour le modèle D_Type."""
+    class Meta:
+        model = D_Type
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,59 +1,35 @@
-"""..."""
 from rest_framework.views import APIView
-from rest_framework import serializers
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from app.models import Club_2, Club
 from api.models import D_AgeGrp, D_Date, D_Federation, D_Localisation, D_Sex, D_Type, F_Club, F_Licence
+from .serializers import (ClubSerializer, Club2Serializer, FLicenceSerializer, FClubSerializer, DAgeGrpSerializer, DDateSerializer, DFederationSerializer, DLocalisationSerializer, DSexSerializer, DTypeSerializer)
+
+TABLE_SERIALIZER_MAP = {
+    'club2': (Club_2, Club2Serializer),
+    'club': (Club, ClubSerializer),
+    'f_licence': (F_Licence, FLicenceSerializer),
+    'f_club': (F_Club, FClubSerializer),
+    'd_agegrp': (D_AgeGrp, DAgeGrpSerializer),
+    'd_date': (D_Date, DDateSerializer),
+    'd_federation': (D_Federation, DFederationSerializer),
+    'd_localisation': (D_Localisation, DLocalisationSerializer),
+    'd_sex': (D_Sex, DSexSerializer),
+    'd_type': (D_Type, DTypeSerializer),
+}
 
 class EndPointDWH(APIView):
     """..."""
     def get(self, request, format=None):
         """..."""
-        table = request.GET['table']
-        if table == 'club2':
-            queryset = Club_2.objects.all()
-            serializer_class = Club2Serializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'club':
-            queryset = Club.objects.all()
-            serializer_class = ClubSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'f_licence':
-            queryset = F_Licence.objects.all()
-            serializer_class = FLicenceSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'f_club':
-            queryset = F_Club.objects.all()
-            serializer_class = FClubSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'd_agegrp':
-            queryset = D_AgeGrp.objects.all()
-            serializer_class = DAgeGrpSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'd_date':
-            queryset = D_Date.objects.all()
-            serializer_class = DDateSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'd_federation':
-            queryset = D_Federation.objects.all()
-            serializer_class = DFederationSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'd_localisation':
-            queryset = D_Localisation.objects.all()
-            serializer_class = DLocalisationSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'd_sex':
-            queryset = D_Sex.objects.all()
-            serializer_class = DSexSerializer
-            nombre_de_lignes = queryset.count()
-        elif table == 'd_type':
-            queryset = D_Type.objects.all()
-            serializer_class = DTypeSerializer
-            nombre_de_lignes = queryset.count()
-        else:
-            return Response({'message': 'Nom de table invalide'}, status=400)
+        table = request.GET.get('table')
+        if table not in TABLE_SERIALIZER_MAP:
+            return Response({'message': 'Nom de table invalide'}, status=status.HTTP_400_BAD_REQUEST)
+
+        model_class, serializer_class = TABLE_SERIALIZER_MAP[table]
+        queryset = model_class.objects.all()
+        nombre_de_lignes = queryset.count()
 
         paginator = PageNumberPagination()
         paginator.page_size = 1
@@ -69,65 +45,3 @@ class EndPointDWH(APIView):
             'previous': paginator.get_previous_link()
         }
         return Response(result, status=status.HTTP_200_OK)
-
-class ClubSerializer(serializers.ModelSerializer):
-    """..."""
-    class Meta:
-        """..."""
-        model = Club
-        fields = '__all__'
-
-class Club2Serializer(serializers.ModelSerializer):
-    """..."""
-    class Meta:
-        """..."""
-        model = Club_2
-        fields = '__all__'
-
-class FLicenceSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle F_Licence."""
-    class Meta:
-        model = F_Licence
-        fields = '__all__'
-
-class FClubSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle F_Club."""
-    class Meta:
-        model = F_Club
-        fields = '__all__'
-
-class DAgeGrpSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle D_AgeGrp."""
-    class Meta:
-        model = D_AgeGrp
-        fields = '__all__'
-
-class DDateSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle D_Date."""
-    class Meta:
-        model = D_Date
-        fields = '__all__'
-
-class DFederationSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle D_Federation."""
-    class Meta:
-        model = D_Federation
-        fields = '__all__'
-
-class DLocalisationSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle D_Localisation."""
-    class Meta:
-        model = D_Localisation
-        fields = '__all__'
-
-class DSexSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle D_Sex."""
-    class Meta:
-        model = D_Sex
-        fields = '__all__'
-
-class DTypeSerializer(serializers.ModelSerializer):
-    """Serializer pour le modèle D_Type."""
-    class Meta:
-        model = D_Type
-        fields = '__all__'
